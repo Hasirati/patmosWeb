6598f24e34c317ce730ac54e9c6250488c025c9a
Ulia <yliia192@gmail.com>
2024-08-16 12:57:38 +0300

    done without adaptive

---
+++ script.js
@@ -0,0 +1,20 @@
+;(() => {
+	window.onload = () => {
+		let burger = document.querySelector('.burger')
+		let menu = document.querySelector('.header-list')
+		let body = document.querySelector('body')
+
+		burger.addEventListener('click', e => {
+			burger.classList.toggle('active')
+			menu.classList.toggle('active')
+			body.classList.toggle('lock')
+		})
+
+		let list = document.querySelector('.header-list nav')
+		list.addEventListener('click', e => {
+			burger.classList.toggle('active')
+			menu.classList.toggle('active')
+			body.classList.toggle('lock')
+		})
+	}
+})()
